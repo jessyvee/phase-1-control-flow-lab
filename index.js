fc1575702d3ef4a466204fb69f18707b74f406a1
Jesse <jessyvee1@gmail.com>
2023-03-14 00:35:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  if (distance <= 400){
+    return ("This one is on me!");
+  }
+  else if (distance>2000 && distance<=2500){
+    return ("I will gladly take your thirty bucks.");
+    }
+  else (distance > 2500);{
+    return ("No can do.")
+    }    
+  } 
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+    if (city == "NYC"){
+      return ("Ok, sounds good.");
+    }
+    else (city != "NYC");{
+      return ("No go.");
+    }
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  // write your code here!
+    const generous = "generous";
+    switch(tip){
+      case generous:
+        return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+      default:
+        return "Bye.";
+    }
+  }
